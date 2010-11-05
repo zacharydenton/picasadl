bfef76ee4f5915369b13f7f704a0a60320907828
Zach Denton <zacharydenton@gmail.com>
2010-11-05 09:50:44 -0400

    added download() method to Album class

--- picasadl.py
+++ picasadl.py
@@ -5,7 +5,8 @@ import gdata.media
 import gdata.geo
 
 class Album:
-	def __init__(self, title="Untitled", numphotos=0, picasa_id=None):
+	def __init__(self, gd_client=None, title="Untitled", numphotos=0, picasa_id=None):
+		self.gd_client = gd_client
 		self.title = title
 		self.numphotos = int(numphotos)
 		self.picasa_id = picasa_id
@@ -16,6 +17,12 @@ class Album:
 			ending = 's'
 
 		return "%s (contains %i photo%s)" % (self.title, self.numphotos, ending)
+
+	def download(self, location=None):
+		if self.gd_client is not None:
+			return True
+		else:
+			return False
 
 class PicasaDownloader:
 	def __init__(self, email, password, source='picasadl'):
@@ -29,20 +36,30 @@ class PicasaDownloader:
 		self.gd_client.source = self.source
 		self.gd_client.ProgrammaticLogin()
 	
-	def get_albums(self, user):
+	def get_albums(self, user=None):
+		if user is None:
+			user = self.email
 		albums = self.gd_client.GetUserFeed(user=user)
 		results = []
 		for album in albums.entry:
-			results.append(Album(album.title.text, album.numphotos.text, album.gphoto_id.text))
+			results.append(Album(self.gd_client, album.title.text, album.numphotos.text, album.gphoto_id.text))
 		return results
+
+	def download_albums(self, user=None, location=''):
+		if user is None:
+			user = self.email
+		albums = self.get_albums(user)
+		for album in albums:
+			album.download(location)
 
 def main():
 	import settings
 
 	downloader = PicasaDownloader(settings.email, settings.password)
 
-	for album in downloader.get_albums(settings.email):
-		print album
+	for album in downloader.get_albums():
+		if album.download():
+			print album
 
 if __name__ == "__main__":
 	main()
