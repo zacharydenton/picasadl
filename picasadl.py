7ec9e4980c478593afe67826b8c85c2a97e999e8
Zach Denton <zacharydenton@gmail.com>
2010-11-05 10:06:19 -0400

    simplified code - improved object interface

--- picasadl.py
+++ picasadl.py
@@ -5,22 +5,26 @@ import gdata.media
 import gdata.geo
 
 class Photo:
-	def __init__(self, gd_client=None, user=None, title="Untitled", gphoto_id=None):
+	def __init__(self, photo, gd_client=None):
+		self.photo = photo
+		
 		self.gd_client = gd_client
-		self.user = user
-		self.title = title
-		self.gphoto_id = gphoto_id
+
+		self.title = self.photo.title.text
+		self.gphoto_id = self.photo.gphoto_id.text
 	
 	def __str__(self):
 		return "%s" % self.title
 
 class Album:
-	def __init__(self, gd_client=None, user=None, title="Untitled", numphotos=0, gphoto_id=None):
+	def __init__(self, album, gd_client=None):
+		self.album = album
+
 		self.gd_client = gd_client
-		self.user = user
-		self.title = title
-		self.numphotos = int(numphotos)
-		self.gphoto_id = gphoto_id
+
+		self.title = self.album.title.text
+		self.numphotos = int(self.album.numphotos.text)
+		self.gphoto_id = self.album.gphoto_id.text
 	
 	def __str__(self):
 		ending = ''
@@ -38,10 +42,10 @@ class Album:
 		if self.gd_client is None:
 			return
 		photos = gd_client.GetFeed(
-				'/data/feed/api/user/%s/albumid/%s?kind=photo' % 
-				(self.user, self.gphoto_id))
+				'/data/feed/api/user/default/albumid/%s?kind=photo' % 
+				(self.gphoto_id))
 		for photo in photos.entry:
-			 yield Photo(self.gd_client, self.user, photo.title.text, photo.gphoto_id.text)
+			 yield Photo(photo, self.gd_client)
 
 class PicasaDownloader:
 	def __init__(self, email, password, source='picasadl'):
@@ -60,8 +64,7 @@ class PicasaDownloader:
 			user = self.email
 		albums = self.gd_client.GetUserFeed(user=user)
 		for album in albums.entry:
-			yield Album(self.gd_client, user, album.title.text, 
-				album.numphotos.text, album.gphoto_id.text)
+			yield Album(album, self.gd_client)
 
 	def download_albums(self, user=None, location=''):
 		if user is None:
