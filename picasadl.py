cf69615f67bd5b794775923f4f003fe8f8d68935
Zach Denton <zacharydenton@gmail.com>
2010-11-05 09:44:16 -0400

    can now request a list of user's albums

--- picasadl.py
+++ picasadl.py
@@ -4,10 +4,45 @@ import gdata.photos.service
 import gdata.media
 import gdata.geo
 
-import settings
+class Album:
+	def __init__(self, title="Untitled", numphotos=0, picasa_id=None):
+		self.title = title
+		self.numphotos = int(numphotos)
+		self.picasa_id = picasa_id
+	
+	def __str__(self):
+		ending = ''
+		if self.numphotos != 1:
+			ending = 's'
 
-gd_client = gdata.photos.service.PhotosService()
-gd_client.email = settings.email
-gd_client.password = settings.password
-gd_client.source = 'exampleCo-exampleApp-1'
-gd_client.ProgrammaticLogin()
+		return "%s (contains %i photo%s)" % (self.title, self.numphotos, ending)
+
+class PicasaDownloader:
+	def __init__(self, email, password, source='picasadl'):
+		self.email = email
+		self.password = password
+		self.source = source
+
+		self.gd_client = gdata.photos.service.PhotosService()
+		self.gd_client.email = self.email
+		self.gd_client.password = self.password
+		self.gd_client.source = self.source
+		self.gd_client.ProgrammaticLogin()
+	
+	def get_albums(self, user):
+		albums = self.gd_client.GetUserFeed(user=user)
+		results = []
+		for album in albums.entry:
+			results.append(Album(album.title.text, album.numphotos.text, album.gphoto_id.text))
+		return results
+
+def main():
+	import settings
+
+	downloader = PicasaDownloader(settings.email, settings.password)
+
+	for album in downloader.get_albums(settings.email):
+		print album
+
+if __name__ == "__main__":
+	main()
